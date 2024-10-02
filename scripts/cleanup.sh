e8cbc7305d65cb95afa8464b273cd22f32429fb5
Cyril <cyril.dohin@talkme.tech>
2024-10-02 11:21:43 +0200

    Refactor cleanup.sh script to update debug messages and remove unnecessary file deletion

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Début du nettoyage du répertoire de destination."
+echo "Début du nettoyage."
 
 # Supprimer le fichier package-lock.json s'il existe
 if [ -f /var/www/html/mon-app/package-lock.json ]; then
-    echo "Suppression de /var/www/html/mon-app/package-lock.json"
+    echo "Suppression de package-lock.json"
     rm /var/www/html/mon-app/package-lock.json
 fi
 
