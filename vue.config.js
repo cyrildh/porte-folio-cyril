8afb0ca285c463fcb71e2cc1d2b5e439f3d0fe05
Cyril <cyril.dohin@talkme.tech>
2024-10-03 10:49:02 +0200

    Refactor vue.config.js to update publicPath for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'  // Remplacez par le nom de votre dépôt GitHub
+    ? '/porte-folio-cyril/' // Remplacez par le nom de votre dépôt
     : '/'
-  })
+}
