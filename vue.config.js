0738a8ca7b9279f16d381d2642e70ebd9a798ef4
Cyril <cyril.dohin@talkme.tech>
2024-10-03 11:53:16 +0200

    Refactor vue.config.js to update publicPath for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-// vue.config.js
 module.exports = {
-  publicPath: '/', // Racine pour AWS
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' // Remplacez par le nom de votre dépôt
+    : '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   // Autres configurations si nécessaire
