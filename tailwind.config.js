ceff12b5b6529e33c43735bedc608b5e0eed925f
Cyril <cyril.dohin@talkme.tech>
2024-10-03 12:12:05 +0200

    Refactor tailwind.config.js to update purge configuration for production deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './public/**/*.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+  },
   theme: {
     extend: {
       colors: {
