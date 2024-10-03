5a338d6b9ae8d8615860d1b8e18dc14a1da7f1d6
Cyril <cyril.dohin@talkme.tech>
2024-10-03 10:58:32 +0200

    Refactor package.json and vue.config.js for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/porte-folio-cyril/' // Remplacez par le nom de votre dépôt
-    : '/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  // Autres configurations si nécessaire
 }
