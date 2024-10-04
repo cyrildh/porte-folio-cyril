e5d0eae6fe4a9e55c6b4b56edb4e1273bf31006d
Cyril <cyril.dohin@talkme.tech>
2024-10-04 09:43:48 +0200

    Refactor API endpoint URLs for production deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
       './public/**/*.html',
       './src/**/*.{vue,js,ts,jsx,tsx}',
     ],
-  },
   theme: {
     extend: {
       colors: {
