c24c0e9c8fb48cfa9ad2c26307116562f65bf2a4
cyrildohin <cyril.dohin@gmail.com>
2024-10-08 16:45:21 +0200

    Refactor server.js and ContactComponent.vue to update email functionality endpoint and add CORS options for allowed origin

--- back-end/server.js
+++ back-end/server.js
@@ -18,32 +18,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Logs pour vérifier les variables d'environnement (à retirer en production)
+// **Logs de Débogage**
 console.log('EMAIL_USER:', process.env.EMAIL_USER);
 console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? 'Défini' : 'Non défini');
 
-// Fonction pour créer le transporteur Nodemailer
-const createTransporter = async () => {
-  try {
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER, // Votre adresse e-mail Gmail
-        pass: process.env.EMAIL_PASS, // Mot de passe d'application ou mot de passe Gmail
-      },
-    });
-
-    // Vérifiez la connexion au serveur SMTP
-    await transporter.verify();
-    console.log('Connexion au serveur SMTP réussie');
-    return transporter;
-  } catch (error) {
-    console.error('Erreur lors de la création du transporteur SMTP:', error.message);
-    throw new Error('Échec de la connexion SMTP');
-  }
-};
-
-// Route pour envoyer un email
 app.post('/api/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
@@ -52,18 +30,26 @@ app.post('/api/send-email', async (req, res) => {
     return res.status(400).json({ error: 'Tous les champs sont requis' });
   }
 
+  // Configurer le transporteur Nodemailer avec Gmail
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER, // Votre adresse e-mail Gmail
+      pass: process.env.EMAIL_PASS, // Mot de passe d'application ou mot de passe Gmail
+    },
+    debug: true, // Activer le mode débogage
+  });
+
+  // Configurer le contenu de l'email
+  const mailOptions = {
+    from: process.env.EMAIL_USER, // Adresse authentifiée
+    to: process.env.EMAIL_USER,   // Vous-même
+    subject: `Site portfolio ! Message de ${name}`,
+    text: `Nom: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+    replyTo: email, // Permet de répondre directement à l'utilisateur
+  };
+
   try {
-    const transporter = await createTransporter();
-
-    // Configurer le contenu de l'email
-    const mailOptions = {
-      from: `"${name}" <${process.env.EMAIL_USER}>`, // Adresse de l'expéditeur
-      to: process.env.EMAIL_USER, // Vous-même
-      subject: `Site portfolio ! Message de ${name}`,
-      text: `Nom: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
-      replyTo: email, // Permet de répondre directement à l'utilisateur
-    };
-
     // Envoyer l'email
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: 'Email envoyé avec succès' });
