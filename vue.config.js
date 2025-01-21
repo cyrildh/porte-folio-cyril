9db0c128f2656d5fed9842c06dae7e5218879229
Cyril Dohin <cyril.dohin@gmail.com>
2025-01-21 18:30:48 +0100

    Update proxy configuration to target port 3000 for email sending

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/send-email': {
-          target: 'http://localhost:3003',
+          target: 'http://localhost:3000',
           changeOrigin: true,
       }
     },
